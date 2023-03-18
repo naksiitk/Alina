36e070658aee169da09e7672a1eeea09f755e562
Reithick <reithick@gmail.com>
2023-03-18 19:43:53 +0530

    Merge

--- server/services/gst.js
+++ server/services/gst.js
@@ -2,10 +2,7 @@ require('dotenv').config()
 
 const fetch = require('node-fetch');
 
-module.exports.checkGSTstatus = async(req) => {
-    fetch('https://gstapi.charteredinfo.com/commonapi/v1.1/search?aspid=1720553323&password=Reithick@1&Action=TP&Gstin=33AKMPK5944M1ZN&SearchGstin=' + req.PANorGSTIN)
-    // .then(res => res.json())
-    .then(res => console.log(res));
+
 module.exports.checkGSTstatus = async(req, res) => {
     let status
 
