44c0fbe3a472878930311534a3011534ad7f62c5
Reithick <reithick@gmail.com>
2023-03-18 19:40:05 +0530

    Rest Documentation Updation

--- server/services/gst.js
+++ server/services/gst.js
@@ -4,6 +4,6 @@ const fetch = require('node-fetch');
 
 module.exports.checkGSTstatus = async(req) => {
     fetch('https://gstapi.charteredinfo.com/commonapi/v1.1/search?aspid=1720553323&password=Reithick@1&Action=TP&Gstin=33AKMPK5944M1ZN&SearchGstin=' + req.PANorGSTIN)
-    .then(res => res.json())
+    // .then(res => res.json())
     .then(res => console.log(res));
 };
